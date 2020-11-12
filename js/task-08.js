183ae7cb04dca0d4b253ee7775c21c4f419149bf
Liubomyr <ljubik@ukr.net>
2020-11-12 14:44:22 +0200

    update

--- js/task-08.js
+++ js/task-08.js
@@ -1,41 +1,23 @@
 // ## Завдання 8 - додаткове, виконувати не обов'язково
 'use strict';
 
-// Напиши скрипт створення і очищення колекції елементів. Користувач вводить
-// кількість елементів в `input` і натискає кнопку `Створити`, після чого
-// рендериться колекція. При натисканні на кнопку `Очистити`, колекція елементів
-// очищається.
-
-// Створи функцію `createBoxes(amount)`, яка приймає 1 параметр `amount` -
-// число. Функція створює стільки `div`, скільки вказано в `amount` і додає їх
-// в `div#boxes`.
-
-// Кожен створений div:
-
-// - Має випадковий rgb колір фону
-// - Розміри найпершого div - 30px на 30px
-// - Кожен наступний div після першого, повинен бути ширше і вище попереднього на
-//   10px
-
-// Створи функцію `destroyBoxes()`, яка очищає `div#boxes`.
-
-var render = document.querySelector('[data-action="render"]');
-var destroy = document.querySelector('[data-action="destroy"]');
-var boxes = document.getElementById("boxes");
+let render = document.querySelector('[data-action="render"]');
+let destroy = document.querySelector('[data-action="destroy"]');
+let boxes = document.getElementById("boxes");
 render.addEventListener("click", getAmount);
 destroy.addEventListener("click", destroyBoxes);
 
 function getAmount() {
-  var amount = +document.querySelector("#controls input").value;
+    let amount = +document.querySelector("#controls input").value;
   createBoxes(amount);
 }
 
 function createBoxes(amount) {
-  var basicSize = 30;
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < amount; i++) {
-    var size = basicSize + i * 10;
-    var div = document.createElement("div");
+    let basicSize = 30;
+    let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
     div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
     fragment.appendChild(div);
   }
